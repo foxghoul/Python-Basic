fa691068a346b71539d903032deb0761fa381634
foxghoul <51588202+foxghoul@users.noreply.github.com>
2021-03-04 19:35:32 +0800

    Update 13_exercise_fibo.py

--- codes/13_exercise_fibo.py
+++ codes/13_exercise_fibo.py
@@ -4,4 +4,18 @@ def fibo(N):
     else:
         return fibo(N - 1) + fibo(N - 2)
 
-print(fibo(8))
+# items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]]
+# ruleKey = 'color'
+# ruleValue = 'blue'
+# dic = {"type": 0, "color": 1, "name": 2}
+# print(list(item[dic[ruleKey]] == ruleValue for item in items))
+# print(sum([True, False, False]))
+#
+# for i in items:
+#     continue
+nums = [3,2,3]
+target = 6
+for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+        if nums[i] + nums[j] == target:
+            print([i, j])
